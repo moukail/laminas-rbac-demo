645d57ad8f82d6ea8a8205c41ef11f4e253a43cd
Ismail Moukafih <i.moukafih@gmail.com>
2023-09-25 15:25:06 +0200

    laminas

---
+++ web/config/modules.config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Cache',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Paginator',
+    'Laminas\Session',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'DoctrineModule',
+    'Laminas\Cache\Storage\Adapter\Filesystem',
+    'Laminas\Cache\Storage\Adapter\Memory',
+    'DoctrineORMModule',
+    'Laminas\DeveloperTools',
+    'Application',
+];
